fa3c1e72f2109ddc87ab69f0ca695fe2304f33f2
miracleren <miracleren@gmail.com>
2025-05-15 09:03:16 +0800

    fix bug

--- src/main/java/com/miracleren/NiceUtils.java
+++ src/main/java/com/miracleren/NiceUtils.java
@@ -136,6 +136,25 @@ public class NiceUtils {
     }
 
     /**
+     * 获取两个符号之间的内容
+     *
+     * @param text  源字符串
+     * @param start 开始符号
+     * @param end   结束符号
+     * @return 两个符号之间的内容
+     */
+    public static String getContentBetweenSymbols(String text, String start, String end) {
+        String regex = Pattern.quote(start) + "([\\s\\S]*?)" + Pattern.quote(end);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return null;
+    }
+
+    /**
      * 是否数值
      *
      * @param str
@@ -155,37 +174,5 @@ public class NiceUtils {
         }
     }
 
-    /**
-     * 是否浮点
-     *
-     * @param str
-     * @return
-     */
-    public static boolean isFloat(String str) {
-        try {
-            Float.parseFloat(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
 
-    /**
-     * 获取两个符号之间的内容
-     *
-     * @param text  源字符串
-     * @param start 开始符号
-     * @param end   结束符号
-     * @return 两个符号之间的内容
-     */
-    public static String getContentBetweenSymbols(String text, String start, String end) {
-        String regex = Pattern.quote(start) + "([\\s\\S]*?)" + Pattern.quote(end);
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(text);
-
-        if (matcher.find()) {
-            return matcher.group(1);
-        }
-        return null;
-    }
 }
